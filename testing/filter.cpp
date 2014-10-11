80fdaf918599e2cc3e6ec58db63f637aa3cb0343
athepvon <dthepvongsa@gmail.com>
2014-10-11 00:33:08 -0500

    here is the folder with all the ones i was working on right now it is working for me but there was something i had to do to get it to compile

---
+++ testing/filter.cpp
@@ -0,0 +1,70 @@
+#include "systemc.h"
+#include "filter.h"
+#include <iostream>
+using namespace std;
+int blue[960][1280],green[960][1280],red[960][1280];
+
+void filter::get_pic(){
+	int col_size = max_cols.read().to_int();
+	int row_size = max_rows.read().to_int();
+	bool notFinish = true;
+	int rows = 0, cols = 0;
+	
+	while(notFinish) {
+
+		//cout << "come in" << endl;
+		do wait(); while(!newimage);
+		//cout << "got the newimage" << endl;
+		ready = true;
+		wait();
+		
+		//ready = false;
+		//wait();
+		do wait();while(!sending);
+		ready = false;
+		wait();
+		
+		//cout << "got the sending" << endl;
+		byte_to_pix = RGB_24_bit.read();
+		wait();
+		
+		//loop = false;
+		//wait();
+
+		//cout << "cols in filter " << cols << endl;
+		//cout << "rows in filter " << rows << endl;	
+
+		blue[rows][cols] = byte_to_pix.range(7,0);
+		//cout << "blue: " << blue[rows][cols] << endl;
+		wait();
+		
+		green[rows][cols] = byte_to_pix.range(15,8);
+		//cout << "green: " << green[rows][cols] << endl;
+		wait();
+		
+		red[rows][cols] = byte_to_pix.range(23,16);
+		//cout << "red: " << red[rows][cols] << endl;
+		wait();
+
+		cols++;					
+		
+		//cout << "rows : " << rows << endl;
+		//cout << "cols : " << cols << endl;
+		ready = true;
+		wait();
+
+		if (cols >= col_size ) {
+			cols = 0;
+			rows++;
+			
+		}
+		wait();	
+		if(rows >= row_size){
+			rows = 0;
+			notFinish = false;
+			cout << "inside row if" << endl;
+		}
+		wait();
+		//loop = true;
+	}
+}
---
+++ testing/input.h
@@ -0,0 +1,18 @@
+#include "systemc.h"
+
+SC_MODULE(input) {
+	
+	sc_in_clk clock;
+	sc_out<bool> newimage,sending,start; //newimage verifies there is a new image. 
+										//sending: verfies data sending, start: verifies to start the input module
+	sc_out<sc_uint<24> > RGB_24_bit; //24 bit rgb pixel port
+	sc_in<bool> ready;	//signal from filter module to start the transfer
+	sc_in<sc_int<16> >max_cols;
+	sc_in<sc_int<16> >max_rows;
+
+	void send_pixel();
+	
+	SC_CTOR(input) {
+		SC_CTHREAD(send_pixel, clock.pos());
+	}
+};
