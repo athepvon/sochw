26488427fe81b1ab9083e4ef6970a0215ec7fc42
athepvon <dthepvongsa@gmail.com>
2014-12-14 21:25:18 -0600

    updating

--- compile_speed_hw.sh
+++ compile_speed_hw.sh
@@ -1,11 +1,11 @@
-export PATH=$PATH:/opt/pkg/petalinux-v2014.2-final/tools/linux-i386/arm-xilinx-linux-gnueabi/bin
+export PATH='/opt/pkg/petalinux-v2014.2-final/tools/linux-i386/arm-xilinx-linux-gnueabi/bin':$PATH
 #export CROSS_COMPILE=arm-xilinx-linux-gnueabi-
-
-arm-xilinx-linux-gnueabi-g++ -o speed speedTracker.cpp \
-			     -Wl,-rpath-link, usr/lib/lib*.so.* \
-                             -L usr/opencv-2.4.2/lib \
-                             -I usr/opencv-2.4.2/include/opencv \
-                             -I usr/opencv-2.4.2/include \
-                             -I usr/drivers/sdi \
-                             -I usr/drivers/car_hardware \
-                             -lopencv_imgproc -lopencv_core -lopencv_highgui -lopencv_video
+arm-xilinx-linux-gnueabi-g++ -o speed speedTracker.cpp `pkg-config --cflags --libs opencv`
+#arm-xilinx-linux-gnueabi-g++ -o speed speedTracker.cpp \
+#			     -Wl,-rpath-link, usr/lib/lib*.so.* \
+#                             -L arm_build/lib \
+#                             -I arm_build/include/opencv \
+#                             -I arm_build/include \
+#                             -I usr/drivers/sdi \
+#                             -I usr/drivers/car_hardware \
+#                             -lopencv_imgproc -lopencv_core -lopencv_highgui -lopencv_video
